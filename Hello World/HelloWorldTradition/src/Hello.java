121690a830f73be2a8af93d2a1dccc38785f64b1
brunoreyes <bruno619reyes@gmail.com>
2020-11-30 00:05:54 -0600

    started basic syntax for java, installed eclipse, and wrote a class

---
+++ Hello World/HelloWorldTradition/src/Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+
+}
+// Java programs like other languages have keywords/ Each has a specific meaning and sometimes they need to be used in a specific order.
+//Java is written using a specific set of rules
+
+// Public, public, and PUBLIC are all considered different keywords
+
+// Public and class are keywords.
+// The public Java keyword is an access modifier. An access modifier allows us to define the scope or how other parts of your 
+//code can access this code. 
+//Public is an access modifier to give full access. 
+
+// the class keyword is used to define a class with the name of the class following the class keyword
+// class Hello for example
+// Hello is the name of the class that when coupled with {}, defines the class block.
