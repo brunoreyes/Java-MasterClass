a7792f7daf3062c53bee1b2bcfdf95e4df5709fd
brunoreyes <bruno619reyes@gmail.com>
2020-12-23 23:58:34 -0600

    Working on a challenge to allow multiple body fields of the same fields. Learned about enums and how they are used to group constants together and use unique types to declare the constants. Used instanceOf() method to find out if certain objects belonged to a list

--- SetsAndHashSets/src/com/company/HeavenlyBody.java
+++ SetsAndHashSets/src/com/company/HeavenlyBody.java
@@ -3,15 +3,29 @@ package com.company;
 import java.util.HashSet;
 import java.util.Set;
 
-public final class HeavenlyBody {
+public  class HeavenlyBody { // took away final keyword to be able to create subclasses
     private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
+    private final BodyTypes bodyType;
 
-    public HeavenlyBody(String name, double orbitalPeriod) {
+    public enum BodyTypes { // enum, Java's way of grouping constants together
+                            // defining a type of BodyTypes, enums use types accepting valid types
+                            // enum gives an error at compile time
+                            // nested enums are automatically static
+        STAR,
+        PLANET,
+        DWARF_PLANET,
+        MOON,
+        COMET,
+        ASTEROID
+    }
+
+    public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
         this.name = name;
         this.orbitalPeriod = orbitalPeriod;
         this.satellites = new HashSet<>();
+        this.bodyType = bodyType;
     }
 
     public String getName() {
@@ -22,7 +36,12 @@ public final class HeavenlyBody {
         return orbitalPeriod;
     }
 
-    public boolean addMoon(HeavenlyBody moon){ // option to add a moon to the HeavenlyBody
+    public BodyTypes getBodyType() {
+        return bodyType;
+    }
+
+    public boolean addSatellite(HeavenlyBody moon){ // option to add a moon to the HeavenlyBody
+        if (moon.getBodyType() == bodyType.MOON)
         return this.satellites.add(moon);
     }
 
@@ -32,24 +51,37 @@ public final class HeavenlyBody {
 
 
     @Override // if I get the method signature wrong the code won't compile
-    public boolean equals(Object object){ // changed HeavenlyBody type to Object
+    // marked method as final in case equals aren't symmetric
+    public final boolean equals(Object object){ // changed HeavenlyBody type to Object
         if (this == object){        // to override the method and not overload it
             return true;            // with a different parameter
         }
-        // implemented equals and hashcode to get troubleshooting code below to work
-        System.out.println("object.getClass ()  is " + object.getClass());
-        System.out.println("this.getClass ()  is " + this.getClass());
-        if((object == null) || (object.getClass() != this.getClass())){
-            return false; // null or doesn't equal
+        if (object instanceof HeavenlyBody){
+            HeavenlyBody theObject = (HeavenlyBody) object;
+            if (this.name.equals(theObject.getName())){
+                return this.bodyType == theObject.getBodyType();
+            }
         }
-        String objectName = ((HeavenlyBody)object).getName();
-        return this.name.equals(objectName);
+        return false;
+        // implemented equals and hashcode to get troubleshooting code below to work
+//        System.out.println("object.getClass ()  is " + object.getClass());
+//        System.out.println("this.getClass ()  is " + this.getClass());
+//        if((object == null) || (object.getClass() != this.getClass())){
+//            return false; // null or doesn't equal
+//        }
+//        String objectName = ((HeavenlyBody)object).getName();
+//        return this.name.equals(objectName);
     }
 
     @Override
-    public int hashCode() {
+    public String toString() {
+        return this.name +": " + this.bodyType +", "+ this.orbitalPeriod; // Java implicitly calls to string method
+    }                                                                       // with concatenation
+
+    @Override
+    public final int hashCode() {
         System.out.println("HashCode called");
-        return this.name.hashCode() + 57; // generate hashCodes that when compare objects using
+        return this.name.hashCode() + 57 + this.bodyType.hashCode(); // generate hashCodes that when compare objects using
         // equals will have the same hashCode
     }
 }
