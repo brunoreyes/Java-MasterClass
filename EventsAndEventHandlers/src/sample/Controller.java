2cbc0ae8e761f9eadcbaf02f02770d79440af50d
brunoreyes <bruno619reyes@gmail.com>
2020-12-27 12:52:02 -0600

    Created methods within Controller to check when either of two buttons were clicked, disabling buttons if nothing is entered into the name text field using .isEmpty() and .trim() to ignore white space or entries of just spaces and no text. Initialized disabling buttons and utilized @FXML to annotate instance variables and methods

--- EventsAndEventHandlers/src/sample/Controller.java
+++ EventsAndEventHandlers/src/sample/Controller.java
@@ -1,6 +1,8 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 
@@ -8,9 +10,35 @@ public class Controller {
     @FXML // type @FXML assigning text field to the name instance var
     // by annotating the instance var declaration, and accepting the import
     private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
 
     @FXML
-    public void onButtonClicked(){
-        System.out.println("Hello, " + nameField.getText());
+    public void initialize(){
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent e){
+        if (e.getSource().equals(helloButton)){
+            System.out.println("Hello, " + nameField.getText());
+        } else if (e.getSource().equals(byeButton)){
+            System.out.println("Bye, " + nameField.getText());
+        }
+//        System.out.println("Hello, " + nameField.getText());
+//
+//        // getting the memory location that changes each time the button is clicked
+//        System.out.println("The following button was pressed: " + e.getSource());
+    }
+
+    @FXML
+    public void handleKeyReleased(){
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty(); // .trim() ignores whitespace
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
     }
 }
