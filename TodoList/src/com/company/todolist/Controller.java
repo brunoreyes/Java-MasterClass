80fd55837bc0d11d3b4141f6a3b9c09557ede12e
brunoreyes <bruno619reyes@gmail.com>
2020-12-27 14:13:14 -0600

    Starting to use the Controller to handle interactions between user interface and data model. Creating a todo app and have created a package to keep the data of the todo list items in. Utilized LocalDate to keep track of the item's due date as well as adding a short description and details of each item. Within the fxml file I am starting to build out the skeleton of the app

---
+++ TodoList/src/com/company/todolist/Controller.java
@@ -0,0 +1,41 @@
+package com.company.todolist;
+
+import com.company.todolist.datamodel.TodoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller { // the Controller handles interaction between UI and data model
+    private List<TodoItem> todoItems;
+
+    public void initialize(){
+        TodoItem item1 = new TodoItem("Mail birthday card",
+                "Buy a 'Happy 30th birthday' card for John",
+                LocalDate.of(2016, Month.APRIL, 25));
+
+        TodoItem item2 = new TodoItem("Doctor's Appointment",
+                "See Dr. Smith at 123 Main St. Bring paperwork",
+                LocalDate.of(2018, Month.MAY, 25));
+
+        TodoItem item3 = new TodoItem("Finish design proposal for Toyota",
+                "Promised Mike, I'd email website mockups by Friday 22nd April",
+                LocalDate.of(2022, Month.JANUARY, 22));
+
+        TodoItem item4 = new TodoItem("Mail mom's gift ",
+                "Mail mom's mother's day flowers",
+                LocalDate.of(2021, Month.APRIL, 20));
+
+        TodoItem item5 = new TodoItem("Buy groceries",
+                "Preferably from Trader Joe's",
+                LocalDate.of(2020, Month.DECEMBER, 28));
+
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+    }
+}
