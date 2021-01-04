e09d7366aeb54138f3ff4ed64b5d23115076a5ad
brunoreyes <bruno619reyes@gmail.com>
2021-01-04 17:14:16 -0600

    Worked on creating porducer and consumer threads, learning how to prevent deadlocks, ad utilize wait, notify, and notifyAll methods

--- Messages/src/com/company/Main.java
+++ Messages/src/com/company/Main.java
@@ -6,6 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
 	    // produce and consume messages
+
+        // issue is once one of the threads start looping the other one can't change the value of empty
+        // because the thread is blocked. Only one synchronized method can run at a time.
+        // So the thread that is looping is holding the lock for the message object, and the other one is blocked,
+        // waiting for the other thread to release the lock. This situation is called a deadlock.
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
     }
 }
 
@@ -15,18 +23,42 @@ class Message{
 
     public synchronized String read(){
         while (empty){
+            try {
+                wait();
+            } catch (InterruptedException e){
 
+            }
         }
         empty = true;
+        notifyAll();
         return message;
     }
 
     public synchronized void write(String message){
         while (!empty){
+            try {
+                wait(); // call wait within a loop so when it returns, we'll go back to the beginning of the loop
+                // never assume a thread has been woken up because the condition that's waited on will be changed.
 
+                // Now each Thread now waits and releases it's lock on the message object when the loop condition
+                // passes, giving the other thread the opportunity to run.
+
+                // Now able to process the message and change the value of the empty var. When it calls the notifierAll
+                // method, the thread that is waiting is now able to proceed. The two thread go back and forth like this
+                // until all the messages have been printed.
+
+                // Atomic operations in Java that happen all at once, and a thread can't be suspended in the middle of them.
+                // Atomic operations: reading & writing reference variables, primitive var except Long and doubles
+
+                // Array list is not thread safe
+
+            } catch (InterruptedException e){
+
+            }
         }
         empty = false;
         this.message = message;
+        notifyAll();
     }
 }
 class Writer implements Runnable {
@@ -52,5 +84,26 @@ class Writer implements Runnable {
             }
         }
         message.write("Finished");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message){
+        this.message = message;
+    }
+    public void run(){
+        Random random = new Random();
+        // looping through the messages received, searching for the last message which is Finished
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished") ;
+             latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e){
+
+            }
+        }
     }
 }
