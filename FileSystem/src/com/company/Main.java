d15d49060cbd3821d8b1220cb962b4498d9a492f
brunoreyes <bruno619reyes@gmail.com>
2021-01-03 18:16:33 -0600

    Practiced using .getPath, .getDefault().getPath() and .get() methods to grab paths from within the working directory, a sub directory, and outside of the project's directory

---
+++ FileSystem/src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // path interface used to identify a file on a file system
+
+        // the character used to separate directory names in a path is called a delimiter (/) on Unix
+
+        // file paths can be absolute, relative. absolute paths start with a root node like C:\ or users
+        // otherwise the path is considered relative
+
+        // new directory is the same as new folder bc directory means folder
+
+        // OutThere one folder outside of project path
+        // Working Directory
+        // SubDirectory
+
+        // creating a path and calling the print file method with the relative path in it
+        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+        printFile(path);
+
+        System.out.println(); // adding space
+
+        // passing a 2nd parameter files to get path within working project: files/SubDirectoryFile.txt
+        Path filePath = FileSystems.getDefault().getPath("files","SubDirectoryFile.txt");
+        printFile(filePath);
+
+        System.out.println();
+
+        // grabbing a file outside of project path's
+        filePath = Paths.get("/Users/brunoreyes/Desktop/Code/Java/Java-MasterClass/OutThereFile.txt");
+        printFile(filePath);
+    }
+
+    private static void printFile(Path path){
+        // read the contents of a file and prints it on the screen
+        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = fileReader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
