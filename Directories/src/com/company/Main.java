c3bcfe66953b56855a12859c605b8b00759cef45
brunoreyes <bruno619reyes@gmail.com>
2021-01-03 21:18:03 -0600

    Accessed local device file seperator preferences to allow windows and OS operating systems to work with code

--- Directories/src/com/company/Main.java
+++ Directories/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
@@ -18,8 +19,8 @@ public class Main {
 //                }
 //        };
 
-        // Creating directory path
-        Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
+        // Creating directory path using file separator to get user's OS preference for file separator
+        Path directory = FileSystems.getDefault().getPath("FileTree" + File.separator + "Dir2");
 
         // Creating the directory's steam and returning a stream of paths
 //        try(DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
@@ -34,6 +35,19 @@ public class Main {
                 System.out.println(file.getFileName()); // Dir3
             } // pipe character | is a byte wise inclusive "or"
         } catch (IOException | DirectoryIteratorException e){
+            System.out.println(e.getMessage());
+        }
+
+        // getting the file separator for the operating system a user is using to run your java code
+        String separator = File.separator; // OS: / windows: \
+        System.out.println(separator);
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println(separator);
+
+        try {
+            Path tempFile = Files.createTempFile("myapp",".appext");
+            System.out.println("Temporary file path = " + tempFile.toAbsolutePath());
+        } catch (IOException e){
             System.out.println(e.getMessage());
         }
 
