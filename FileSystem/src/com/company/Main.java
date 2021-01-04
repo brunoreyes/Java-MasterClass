c2bb5f4f23065f6ef9707f27f755022cfb62b8e9
brunoreyes <bruno619reyes@gmail.com>
2021-01-03 18:26:58 -0600

    Using .get to either write out the full absolute path in oneString or splitting the path into current directory, sub folder, and file name

--- FileSystem/src/com/company/Main.java
+++ FileSystem/src/com/company/Main.java
@@ -23,6 +23,7 @@ public class Main {
         // Working Directory
         // SubDirectory
 
+        // .getDefault is more convenient to get paths from within project directory
         // creating a path and calling the print file method with the relative path in it
         Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
         printFile(path);
@@ -30,14 +31,25 @@ public class Main {
         System.out.println(); // adding space
 
         // passing a 2nd parameter files to get path within working project: files/SubDirectoryFile.txt
-        Path filePath = FileSystems.getDefault().getPath("files","SubDirectoryFile.txt");
+//        Path filePath = FileSystems.getDefault().getPath("files","SubDirectoryFile.txt");
+
+//        .get(currentDirectory, file's directory (the subFolder), fileNameItself)
+        Path filePath = Paths.get(".", "files", "SubDirectoryFile.txt");
         printFile(filePath);
 
         System.out.println();
 
-        // grabbing a file outside of project path's
+//        .get(currentDirectory, file's directory (the subFolder), fileNameItself)
+        // grabbing a file outside of project path's (specified the absolute path)
         filePath = Paths.get("/Users/brunoreyes/Desktop/Code/Java/Java-MasterClass/OutThereFile.txt");
+
+//        filePath = Paths.get("Users/brunoreyes/Desktop/Java","Java-MasterClass","OutThereFile.txt");
+
         printFile(filePath);
+
+        // getting the absolute path of this project from the console
+        filePath = Paths.get(".");
+        System.out.println(filePath.toAbsolutePath());
     }
 
     private static void printFile(Path path){
