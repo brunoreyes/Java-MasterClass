63bd1a6dcfe847f883581ad6953fae199d9269c7
brunoreyes <bruno619reyes@gmail.com>
2021-01-04 22:34:13 -0600

    Implemented buffer.unlock and lock methods. Reviewed the importance of ensuring threads are being unlocked or else threads waiting for the lock will be blocked forever

--- ProducerConsumer/src/com/company/Main.java
+++ ProducerConsumer/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
@@ -13,9 +14,13 @@ public class Main {
         // Consumer: Thread that reads data from the buffer
 
         List<String> buffer = new ArrayList<String>();
-        MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_PURPLE);
-        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN); // utilizing private packages
-        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_GREEN);
+
+        // lock object, using instance of ReentrantLock class
+        ReentrantLock bufferLock = new ReentrantLock();
+
+        MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferLock); // utilizing private packages
+        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_GREEN, bufferLock);
 
         new Thread(producer).start();
         new Thread(consumer1).start();
@@ -26,10 +31,12 @@ public class Main {
 class MyProducer implements Runnable {
     private List<String> buffer;
     private String color;
+    private ReentrantLock bufferLock;
 
-    public MyProducer(List<String> buffer, String color) {
+    public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
         this.buffer = buffer;
         this.color = color;
+        this.bufferLock = bufferLock;
     }
     public void run(){
         Random random = new Random();
@@ -37,9 +44,13 @@ class MyProducer implements Runnable {
         for ( String num: nums){
             try {
                 System.out.println(color + "Adding...");
-                synchronized (buffer){
+//                synchronized (buffer){
+                bufferLock.lock(); // lock() acquires the lock
                     buffer.add(num);
-                }
+//                }
+                bufferLock.unlock(); // unlock() releases the lock
+                // forgetting to release the lock, threads waiting for the lock
+                // will start locking forever.
 
 
                 Thread.sleep(random.nextInt(1000));
@@ -48,9 +59,11 @@ class MyProducer implements Runnable {
             }
         }
         System.out.println(color + "Adding EOF & exiting...");
-        synchronized (buffer){
+        bufferLock.lock();
+//        synchronized (buffer){
             buffer.add("EOF");// letting consumers know there won't be any more data to process.
-        }
+//        }
+        bufferLock.unlock();
     }
 }
 
@@ -58,45 +71,69 @@ class MyConsumer implements Runnable {
 
     private List<String> buffer;
     private String color;
+    private ReentrantLock bufferLock;
 
-    MyConsumer(List<String> buffer, String color){
+
+    MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock){
         this.buffer = buffer;
         this.color = color;
+        this.bufferLock = bufferLock;
     }
     public void run(){
         while (true){
-            synchronized (buffer){ // thread interference no longer occurs thanks to synchronized
-
+          //  synchronized (buffer){ // thread interference no longer occurs thanks to synchronized
+                bufferLock.lock();
                 if (buffer.isEmpty()){
+                    bufferLock.unlock();
                     continue;
                 }
                 if (buffer.get(0).equals(Main.EOF)){
                     System.out.println(color + "Exiting");
+                    bufferLock.unlock();
                     break;
                 } else {
                     System.out.println(color + "Removed " + buffer.remove(0));
                 }
-            }
+                bufferLock.unlock();
+//            }
 
         }
+//        ReentrantLock lock1:
+//        public void methodA(){
+//            lock1.lock();
+//            methodB();
+//            [more code]
+//        lock1.unlock();
+//        }
+//        public void methodB(){
+//            [more code]
+//            lock1.lock();
+//            methodB();
+//            [more code]
+//            lock1.unlock(); // when methodB calls unlock we don't want any other threads to
+        // get lock1 bc method A still expects to be holding the lock.
+
+        // Bc the same thread can acquire the same lock multiple times, the unlock method won't
+        // release the lock until the hold count reaches 0.
     }
-    // drawbacks for synchronized code:
-
-    // First drawback is that threads that are blocked
-    //waiting to execute synchronize code can't be interrupted
-    //once they're blocked their stuck there until they get the lock for the object
-    //the code is synchronizing on and if you think about it that can lead to
-    //problems.
-
-    // Second drawback is that the synchronized block must be within the same method in other
-    //words we can't start a synchronized block in one method and end the
-    //synchronization block in another for obvious reasons
-
-    //Third drawback is that we can't test to see if an object's intrinsic lock is
-    //available or find out any other information about that lock
-    //also if the lock isn't available we can't timeout after we waited for the
-    //lock for a while when we reach the beginning of a synchronized block
-    //we can either get the lock and continue executing or block at that line of code
-    //until we get the lock and the fourth drawback is that if multiple threads are
-    //waiting to get a lock
 }
+
+// drawbacks for synchronized code:
+
+// First drawback is that threads that are blocked
+//waiting to execute synchronize code can't be interrupted
+//once they're blocked their stuck there until they get the lock for the object
+//the code is synchronizing on and if you think about it that can lead to
+//problems.
+
+// Second drawback is that the synchronized block must be within the same method in other
+//words we can't start a synchronized block in one method and end the
+//synchronization block in another for obvious reasons
+
+//Third drawback is that we can't test to see if an object's intrinsic lock is
+//available or find out any other information about that lock
+//also if the lock isn't available we can't timeout after we waited for the
+//lock for a while when we reach the beginning of a synchronized block
+//we can either get the lock and continue executing or block at that line of code
+//until we get the lock and the fourth drawback is that if multiple threads are
+//waiting to get a lock
