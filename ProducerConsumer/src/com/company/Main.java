5cc5b596ee708f2af523785b369a8d16f07b6f79
brunoreyes <bruno619reyes@gmail.com>
2021-01-04 22:13:06 -0600

    Utilized synchronized method on bugger to avoid thread interference. Reviewed some drawbacks when using synchronized

--- ProducerConsumer/src/com/company/Main.java
+++ ProducerConsumer/src/com/company/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
         List<String> buffer = new ArrayList<String>();
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_PURPLE);
-        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN); // utilizing private packages
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_GREEN);
 
         new Thread(producer).start();
@@ -37,7 +37,10 @@ class MyProducer implements Runnable {
         for ( String num: nums){
             try {
                 System.out.println(color + "Adding...");
-                buffer.add(num);
+                synchronized (buffer){
+                    buffer.add(num);
+                }
+
 
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e){
@@ -45,7 +48,9 @@ class MyProducer implements Runnable {
             }
         }
         System.out.println(color + "Adding EOF & exiting...");
-        buffer.add("EOF");// letting consumers know there won't be any more data to process.
+        synchronized (buffer){
+            buffer.add("EOF");// letting consumers know there won't be any more data to process.
+        }
     }
 }
 
@@ -60,15 +65,38 @@ class MyConsumer implements Runnable {
     }
     public void run(){
         while (true){
-            if (buffer.isEmpty()){
-                continue;
+            synchronized (buffer){ // thread interference no longer occurs thanks to synchronized
+
+                if (buffer.isEmpty()){
+                    continue;
+                }
+                if (buffer.get(0).equals(Main.EOF)){
+                    System.out.println(color + "Exiting");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
             }
-            if (buffer.get(0).equals(Main.EOF)){
-                System.out.println(color + "Exiting");
-                break;
-            } else {
-                System.out.println(color + "Removed " + buffer.remove(0));
-            }
+
         }
     }
+    // drawbacks for synchronized code:
+
+    // First drawback is that threads that are blocked
+    //waiting to execute synchronize code can't be interrupted
+    //once they're blocked their stuck there until they get the lock for the object
+    //the code is synchronizing on and if you think about it that can lead to
+    //problems.
+
+    // Second drawback is that the synchronized block must be within the same method in other
+    //words we can't start a synchronized block in one method and end the
+    //synchronization block in another for obvious reasons
+
+    //Third drawback is that we can't test to see if an object's intrinsic lock is
+    //available or find out any other information about that lock
+    //also if the lock isn't available we can't timeout after we waited for the
+    //lock for a while when we reach the beginning of a synchronized block
+    //we can either get the lock and continue executing or block at that line of code
+    //until we get the lock and the fourth drawback is that if multiple threads are
+    //waiting to get a lock
 }
