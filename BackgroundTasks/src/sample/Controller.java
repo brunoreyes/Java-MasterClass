5afc09aeb62709ebce00741c3a739dbd26ba7835
brunoreyes <bruno619reyes@gmail.com>
2021-01-05 22:45:03 -0600

    Utilized .setOnRunning and .setOnSucceeded methods to change visibility state of progress bar and label depending on which state the service was in. Switched out long way of doing it in place for data binding binding the progress property to the service property

--- BackgroundTasks/src/sample/Controller.java
+++ BackgroundTasks/src/sample/Controller.java
@@ -6,6 +6,8 @@ import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Worker;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -14,7 +16,7 @@ import javafx.scene.control.ProgressBar;
 public class Controller {
 
 //    private Task<ObservableList<String>> task;
-    
+
     @FXML // annotating with @FXML
     private ListView listView; // able to update the listview using the platform.run later method
     // accepting a runnable and runs it on the UI thread.
@@ -40,17 +42,40 @@ public class Controller {
 //            }
 //        };
 
-
         service = new EmployeeService();
 
         // linking the progress bar to the task's progress using data binding.
         progressBar.progressProperty().bind(service.progressProperty()); // switched task to service
         // Rather than using platform.runLater(), I should use data binding, to bind the items property to
         // the tasks property. Here I'm binding the tasks property to the item's property.
-
         progressLabel.textProperty().bind(service.messageProperty());
         listView.itemsProperty().bind(service.valueProperty());
         // So I'm performing a long-running task on the background thread and updating the UI thread.
+
+        // if service is running
+        progressBar.visibleProperty().bind(service.runningProperty());
+        progressLabel.visibleProperty().bind(service.runningProperty());
+
+        // Or the commented code below
+//        service.setOnRunning(new EventHandler<WorkerStateEvent>() {
+//            @Override
+//            public void handle(WorkerStateEvent event) {
+//                progressBar.setVisible(true);
+//                progressLabel.setVisible(true);
+//            }
+//        });
+//
+//        // if service has succeeded
+//        service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+//            @Override
+//            public void handle(WorkerStateEvent event) {
+//                progressBar.setVisible(false);
+//                progressLabel.setVisible(false);
+//            }
+//        });
+//        // when running for the first time
+//        progressBar.setVisible(false);
+//        progressLabel.setVisible(false);
 
 
     }
@@ -64,6 +89,5 @@ public class Controller {
             service.start();
         }
 //        new Thread(task).start();
-        service.start();
     }
 }
