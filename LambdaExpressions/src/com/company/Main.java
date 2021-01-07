523dee2c20ee1c46a56cfe56e60d4a49dc1341a8
brunoreyes <bruno619reyes@gmail.com>
2021-01-06 18:07:27 -0600

    Created a lambda expression that takes two strings, combines them and makes the combined string uppercase

--- LambdaExpressions/src/com/company/Main.java
+++ LambdaExpressions/src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -40,7 +37,7 @@ public class Main {
 //        });
         // Here the Lambda expression is being passed as a second parameter instead
         // of being an anonymous comparator.
-        // ((Employee employee1, Employee employee2): argument list
+        // ((Employee employee1, Employee employee2): argument list or parameters
         // -> : arrow token
         // body: employee1.getName().compareTo(employee2.getName())
         Collections.sort(employees, ( employee1,  employee2) ->
@@ -50,8 +47,27 @@ public class Main {
             System.out.println(employee.getName());
         }
 
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String UpperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        },
+//        employees.get(0).getName(),employees.get(1).getName());
+//        System.out.println(sillyString);
+
+        UpperConcat uc = (String s1, String s2) -> s1.toUpperCase() + s2.toUpperCase();
+
+        String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+        System.out.println(sillyString);
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2){
+        return uc.UpperAndConcat(s1, s2);
     }
 }
+
+
 class Employee {
     private String name;
     private int age;
@@ -76,6 +92,10 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface  UpperConcat {
+    public String UpperAndConcat(String s1, String s2);
 }
 
 //class CodeToRun implements Runnable{
