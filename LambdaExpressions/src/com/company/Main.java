1373908cfc3a7c870c50d705a63a2ad10cd03c23
brunoreyes <bruno619reyes@gmail.com>
2021-01-06 19:40:33 -0600

    Implemented a lambda expression within a nested block and was able to utilize variables from the enclosing block

--- LambdaExpressions/src/com/company/Main.java
+++ LambdaExpressions/src/com/company/Main.java
@@ -18,14 +18,14 @@ public class Main {
 //            System.out.format("This is line %d\n",3);
 //        }).start();
 
-        Employee john = new Employee("John Doe", 30);
-        Employee bon = new Employee("Bon John", 10);
-        Employee jillian = new Employee("Jillian Smith", 31);
-
-        List<Employee> employees = new ArrayList<>();
-        employees.add(john);
-        employees.add(bon);
-        employees.add(jillian);
+//        Employee john = new Employee("John Doe", 30);
+//        Employee bon = new Employee("Bon John", 10);
+//        Employee jillian = new Employee("Jillian Smith", 31);
+//
+//        List<Employee> employees = new ArrayList<>();
+//        employees.add(john);
+//        employees.add(bon);
+//        employees.add(jillian);
 
 
         // sorting by A-Z
@@ -40,12 +40,12 @@ public class Main {
         // ((Employee employee1, Employee employee2): argument list or parameters
         // -> : arrow token
         // body: employee1.getName().compareTo(employee2.getName())
-        Collections.sort(employees, ( employee1,  employee2) ->
-                employee1.getName().compareTo(employee2.getName()));
-
-        for (Employee employee: employees) {
-            System.out.println(employee.getName());
-        }
+//        Collections.sort(employees, ( employee1,  employee2) ->
+//                employee1.getName().compareTo(employee2.getName()));
+//
+//        for (Employee employee: employees) {
+//            System.out.println(employee.getName());
+//        }
 
 //        String sillyString = doStringStuff(new UpperConcat() {
 //            @Override
@@ -56,14 +56,21 @@ public class Main {
 //        employees.get(0).getName(),employees.get(1).getName());
 //        System.out.println(sillyString);
 
-        UpperConcat uc = (String s1, String s2) -> s1.toUpperCase() + s2.toUpperCase();
+//        UpperConcat uc = (String s1, String s2) -> {
+//            String result = s1.toUpperCase() + s2.toUpperCase();
+//            return  result;
+//        };
+//
+//        String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+//        System.out.println(sillyString);
 
-        String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
-        System.out.println(sillyString);
+        AnotherClass anotherClass = new AnotherClass();
+        String s = anotherClass.doSomething();
+        System.out.println(s);
     }
 
     public final static String doStringStuff(UpperConcat uc, String s1, String s2){
-        return uc.UpperAndConcat(s1, s2);
+        return uc.UpperAndConcat(s1, s2); // calling do something method within another class
     }
 }
 
@@ -97,6 +104,48 @@ class Employee {
 interface  UpperConcat {
     public String UpperAndConcat(String s1, String s2);
 }
+
+class AnotherClass {
+    public String doSomething(){
+//        UpperConcat uc = (s1, s2) -> {
+//            System.out.println("The lambda expression's class is " + getClass().getSimpleName());
+//            String result = s1.toUpperCase() + s2.toUpperCase();
+//            return result;
+//        };
+//        final int i = 0;
+         int i = 0;
+         UpperConcat uc = (s1, s2) -> {
+             System.out.println("The lambda expression's class is " + getClass().getSimpleName());
+             String result = s1.toUpperCase() + s2.toUpperCase();
+             return result;
+         };
+//        {// code within a nest block can reference variables within the enclosing block ( doSomething() )
+//            UpperConcat uc = new UpperConcat() {
+//                @Override
+//                public String UpperAndConcat(String s1, String s2) {
+                    // the var is replaced by whatever the value of i is when it's constructed
+//                    System.out.println("i (within anonymous class) = "+ i);
+//                    return s1.toUpperCase() + s2.toUpperCase();
+//                }
+//            };
+
+//            System.out.println("The anonymous class's name is: " + getClass().getSimpleName());
+//            i++;
+//            System.out.println("i = " + i);
+
+        System.out.println("The AnotherClass's name is: " + getClass().getSimpleName());
+        return Main.doStringStuff(uc,"String1", "String2");
+        }
+
+//        System.out.println("The AnotherClass's name is: " + getClass().getSimpleName());
+//        return Main.doStringStuff(new UpperConcat() {
+//            @Override
+//            public String UpperAndConcat(String s1, String s2) {
+//                System.out.println("The anonymous class's name is: " + getClass().getSimpleName());
+//                return s1.toUpperCase(Locale.ROOT) + s2.toUpperCase(Locale.ROOT);
+//            }
+//        },"String1", "String2");
+    }
 
 //class CodeToRun implements Runnable{
 //    @Override
