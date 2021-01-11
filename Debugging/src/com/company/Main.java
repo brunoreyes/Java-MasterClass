49b2b32457721322f7baa7240bf8b9c0c92ab796
brunoreyes <bruno619reyes@gmail.com>
2021-01-10 23:32:19 -0600

    Going through bottom menu frames and variables sections showing activity of methods run on multiple threads and variables of multiple classes

--- Debugging/src/com/company/Main.java
+++ Debugging/src/com/company/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         // Using IntelliJ's IDE debugger: click debug bug icon next to code run
+        // within debugger on bottom left is the frames section showing the execution stack trace.
+        // everytime a frame is called it is added to the stack trace.
+        // frames are called from bottom first to top last.
+        // multiple threads can go through
+
+        // on the bottom center we see the variables table showing variables of each class
+        // instance variables have an expandable menu ">"
         StringUtilities utils = new StringUtilities();
         StringBuilder sb = new StringBuilder();
         while (sb.length() < 10){
