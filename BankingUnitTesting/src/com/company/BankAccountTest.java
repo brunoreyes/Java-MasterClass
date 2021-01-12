1b36d1fc4243ecd5b8bf00c60e64854bd4e41fbb
brunoreyes <bruno619reyes@gmail.com>
2021-01-12 11:12:32 -0600

    Practicing generating unit test for a class and it's methods and configuring the JUnit library by adding it's resource via 'add to classPath' option.

---
+++ BankingUnitTesting/src/com/company/BankAccountTest.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    // to get rid of errors, press on the error junit with option + enter. and add to classPath.
+    // the same with jupiter for juint5
+    @org.junit.jupiter.api.Test
+    void deposit() throws Exception{
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() throws Exception{
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() throws Exception{
+    }
+
+    // To create a separate run configuration for all the test above, to test class of the application
+    // and not actually run the test itself
+}
---
+++ BankingUnitTesting/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Junit 4 is the latest version of unit testing debugger
+    }
+}
