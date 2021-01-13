eaa18b1a265e7e3d38110ee04e08f15418e72e84
brunoreyes <bruno619reyes@gmail.com>
2021-01-13 14:57:29 -0600

    Preforming CRUD on SQL database using Java and then retriving results back into Java using ResultSets

--- TestDataBase/src/com/company/Main.java
+++ TestDataBase/src/com/company/Main.java
@@ -30,35 +30,58 @@ package com.company;
 // Adding JDBC to a Java Project
 // File > Project Structure > Libraries >> "+" button >> go to JDBC.jar file > add it
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     // All JDBC drivers use a connection string to connect to a DB, using JDBC:
 
     public static void main(String[] args) {
         // Utilized absolute path and changed it so it doesn't go into src but: TestDataBase
-        // Using try & catch with resources to automatically close the resource when try & catch block exits
-//        try (Connection connection = DriverManager.getConnection(
-//                "jdbc:sqlite:/Users/brunoreyes/Desktop/Code/Java/" +
-//                        "Java-MasterClass/TestDataBase/testjava.db");
-//             Statement statement = connection.createStatement()){
-
-        try {
-            Connection connection = DriverManager.getConnection(
+       //  Using try & catch with resources to automatically close the resource when try & catch block exits
+        try (Connection connection = DriverManager.getConnection(
                 "jdbc:sqlite:/Users/brunoreyes/Desktop/Code/Java/" +
                         "Java-MasterClass/TestDataBase/testjava.db");
+             Statement statement = connection.createStatement()){
 
-            Statement statement = connection.createStatement();
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+//        try {
+//            Connection connection = DriverManager.getConnection(
+//                "jdbc:sqlite:/Users/brunoreyes/Desktop/Code/Java/" +
+//                        "Java-MasterClass/TestDataBase/testjava.db");
+            //connection.setAutoCommit(false); // turning off auto commit to not automatically add data
+//            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS" + //  only creates table "IF NOT EXISTS"
+                    " contacts (name TEXT, phone INTEGER, email TEXT)");
             // when calling execute() the driver understands I'm passing a complete query so no ";"
             // semicolon is need at the end of the statement.
 
-            // Remember: Always Close statements before closing connections
-            statement.close();
-            connection.close();
+//            Commenting out Inserts to avoid adding duplicate data
+//            statement.execute("INSERT INTO contacts (name, phone, email)" +
+//                    " VALUES ('Bruno', 1234567890, 'bruno@email.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email)" +
+//                    " VALUES ('Joe', 0987654321, 'joe@email.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email)" +
+//                    " VALUES ('Mojo', 1234567899, 'mojo@email.com')");
+
+            statement.execute("UPDATE contacts SET phone=5566798 WHERE name='Joe'");
+            statement.execute("DELETE FROM contacts WHERE name='Bruno'");
+            statement.execute("SELECT * FROM contacts");
+
+            // Recall to use different statement instances for each query when using or checking data
+            // Above is okay because we are only preforming CRUD and not using or checking data
+            ResultSet results = statement.getResultSet(); // Gathering data from SQL database using ResultSet.
+            while (results.next()){
+                System.out.println(results.getString("name")
+                    + " " + results.getString("phone")
+                    + " " + results.getString("email"));
+            }
+            results.close();
+            
+
+
+            // Connection is connection to db and statement is the statement that you want to execute.
+            // Remember: Always Close statements before closing connections if not using try/catch w/ resources
+//            statement.close();
+//            connection.close();
 
         } catch (SQLException e){
             System.out.println("Something went wrong: " + e.getMessage());
