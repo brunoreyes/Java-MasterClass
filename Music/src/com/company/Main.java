11e6850b811a24e48712b5827849291d41b6104a
brunoreyes <bruno619reyes@gmail.com>
2021-01-13 22:55:04 -0600

    Utilized JDBC driver to be able to open the datasource database

--- Music/src/com/company/Main.java
+++ Music/src/com/company/Main.java
@@ -3,9 +3,13 @@ package com.company;
 import com.company.model.Datasource;
 
 public class Main {
-
+    // Ensure to import JDBC driver via project structure >> library
     public static void main(String[] args) {
         Datasource datasource = new Datasource();
-        if (no)
+        if (!datasource.open()){
+            System.out.println("Can't open datasource");
+            return;
+        }
+        datasource.close();
     }
 }
