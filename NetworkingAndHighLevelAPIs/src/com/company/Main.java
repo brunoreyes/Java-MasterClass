0aae9842d17959c798f81f02fbfd29c7d911d9b0
brunoreyes <bruno619reyes@gmail.com>
2021-01-20 21:00:27 -0600

    combining a base uri with a relative uri to create an absolute URI and converting it to a url because it has an absolute path

--- NetworkingAndHighLevelAPIs/src/com/company/Main.java
+++ NetworkingAndHighLevelAPIs/src/com/company/Main.java
@@ -25,14 +25,25 @@ package com.company;
 
 // URI's that specify schemes are called absolute URIs, else if it doesn't, it's a relative URI
 
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+//            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+            URI baseUri = new URI("http://username:password@myserver.com:5000");
+            URI uri = new URI("/catalogue/phones?os=android#samsung"); // URI is not absolute, can get error message
+            // can't get an absolute URI from a relative URI.
+            // When I want to access a web page on myserver.com host, combining base and relative URIs using resolve()
+            URI resolvedUri = baseUri.resolve(uri);
+
+            URL url = resolvedUri.toURL();
+//            URL url = uri.toURL();
+            System.out.println("URL = " + url);
             //            URI uri = new URI("hello"); //
 // gathering the 9 components
 //            System.out.println("Scheme = " + uri.getScheme());
@@ -44,8 +55,10 @@ public class Main {
 //            System.out.println("Path = " + uri.getPath());
 //            System.out.println("Query = " + uri.getQuery());
 //            System.out.println("Fragment = " + uri.getFragment());
-        } catch (URISyntaxException e){
+        } catch (URISyntaxException  e){
             System.out.println("URI Bad Syntax: " + e.getMessage());
+        } catch (MalformedURLException e){
+            System.out.println("URL Malformed: " + e.getMessage());
         }
 
     }
